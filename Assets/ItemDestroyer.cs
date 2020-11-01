e1a6fc42de80171bedc0dc7df7f1a1bb713efa9c
Hazukimilk <60565426+Hazukimilk@users.noreply.github.com>
2020-11-02 00:02:47 +0900

    Initial commit

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour
+{
+    private GameObject unitychan;
+
+    private float difference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+        if (this.difference > 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
